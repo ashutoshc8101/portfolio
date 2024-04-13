5f280db4ed500b591d550a1d693de85b5cb8de0c
ashutoshc8101 <ashutoshc8101@gmail.com>
2024-04-13 21:14:06 +0530

    fix ci

--- src/app/page.js
+++ src/app/page.js
@@ -25,7 +25,7 @@ export default function Home() {
             {/* <Image src={profile} alt='profile image' /> */}
             <h1 style={styles.nameStyle}>Ashutosh Chauhan</h1>
             <div className='bio'>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             </div>
             <Social />
           </div>
